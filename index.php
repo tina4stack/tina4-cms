71d0f7e3b74194bdf775e740a68cb038d3cf3999
Kobus van Wyk <kobus.vanwyk.84@gmail.com>
2023-09-28 16:13:21 +0200

    Added SCSS compiling

--- index.php
+++ index.php
@@ -10,11 +10,18 @@ $DBA = new \Tina4\DataSQLite3("cms.db");
 
 $config = new \Tina4\Config(function(\Tina4\Config $config) {
     (new Content())->addConfigMethods($config);
+    (new Content())->addCmsMenu("/backend/program", "Products");
+    (new Content())->addCmsMenu("/backend/program", "Something Else");
     //(new Content())->addCmsMenu("/backend/program", "Products");
     // $config->addTwigGlobal("Menu", new Menu());
     //(new Theme())->addTwigView("product", "Products", "examples/products.twig");
     //(new Theme())->addTwigView("menu", "Menu", "examples/menu.twig");
 });
+
+//Hack to build css for documentation
+$scss = new ScssPhp\ScssPhp\Compiler();
+$scssDefault = $scss->compileString(file_get_contents("./src/templates/admin/page-builder.scss"))->getCss();
+file_put_contents("./src/public/css/page-builder.css", $scssDefault);
 
 
 (new Theme(""))->deployThemes(__dir__, true);
