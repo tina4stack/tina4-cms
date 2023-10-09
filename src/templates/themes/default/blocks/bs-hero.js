67b4e4fc384dc020be677d78781881d69e1bb4ff
Kobus van Wyk <kobus.vanwyk.84@gmail.com>
2023-10-09 17:59:29 +0200

    Added all right sidebar icons. Sidebar styling. TODO: Fix right sidebar font colours

--- src/templates/themes/default/blocks/bs-hero.js
+++ src/templates/themes/default/blocks/bs-hero.js
@@ -2,6 +2,7 @@ blockManager.add('bs-hero-centered', {
     category: 'Bootstrap - Heroes',
     label: 'Hero - Centered',
     media: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44.5 44.5"><path d="M0 0h44.5v44.5H0z" style="fill:none;stroke-width:0"/><path d="M25.54 16.5h-6.59c-1.81 0-3.28-1.47-3.28-3.28V6.63c0-1.81 1.47-3.28 3.28-3.28h6.59c1.81 0 3.28 1.47 3.28 3.28v6.59c0 1.81-1.47 3.28-3.28 3.28ZM18.95 5.86c-.43 0-.78.35-.78.78v6.59c0 .43.35.78.78.78h6.59c.43 0 .78-.35.78-.78V6.64c0-.43-.35-.78-.78-.78h-6.59ZM31.86 25.37H12.64c-1.81 0-3.28-1.47-3.28-3.28v-.76c0-1.81 1.47-3.28 3.28-3.28h19.22c1.81 0 3.28 1.47 3.28 3.28v.76c0 1.81-1.47 3.28-3.28 3.28Zm-19.22-4.81c-.43 0-.78.35-.78.78v.76c0 .43.35.78.78.78h19.22c.43 0 .78-.35.78-.78v-.76c0-.43-.35-.78-.78-.78H12.64ZM18.7 41.15H8.12c-1.68 0-3.06-1.37-3.06-3.05s1.37-3.06 3.06-3.06H18.7c1.68 0 3.06 1.37 3.06 3.06s-1.37 3.05-3.06 3.05ZM8.12 37.54c-.31 0-.56.25-.56.56s.25.55.56.55H18.7c.31 0 .56-.25.56-.55s-.25-.56-.56-.56H8.12ZM36.38 41.15H25.8c-1.68 0-3.06-1.37-3.06-3.05s1.37-3.06 3.06-3.06h10.58c1.68 0 3.05 1.37 3.05 3.06s-1.37 3.05-3.05 3.05ZM25.8 37.54c-.31 0-.56.25-.56.56s.25.55.56.55h10.58c.31 0 .55-.25.55-.55s-.25-.56-.55-.56H25.8ZM38.18 29.42H6.32a1.25 1.25 0 0 1 0-2.5h31.87a1.25 1.25 0 0 1 0 2.5ZM38.18 33.48H6.32a1.25 1.25 0 0 1 0-2.5h31.87a1.25 1.25 0 0 1 0 2.5Z" style="fill:#515962;stroke-width:0"/></svg>
     `,
     content: `<div  class="px-4 py-5 my-5 text-center">
                                                 <img class="d-block mx-auto mb-4" src="" alt="" width="72" height="57">
@@ -17,30 +18,11 @@ blockManager.add('bs-hero-centered', {
     `
 });
 
-
-blockManager.add('bs-hero-dark', {
-    category: 'Bootstrap - Heroes',
-    label: 'Hero - Dark',
-    media: `
-    `,
-    content: `<div class="bg-dark text-secondary px-4 py-5 text-center">
-                                                <div class="py-5">
-                                                  <h1 class="display-5 fw-bold text-white">Dark color hero</h1>
-                                                  <div class="col-lg-6 mx-auto">
-                                                    <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world's most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                                                    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                                                      <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-                                                      <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
-                                                    </div>
-                                                  </div>
-                                                </div>
-                                              </div>`
-});
-
 blockManager.add('bs-hero-left', {
     category: 'Bootstrap - Heroes',
     label: 'Hero - Left',
     media: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44.5 44.5"><path d="M0 0h44.5v44.5H0z" style="fill:none;stroke-width:0"/><path d="M37.96 34.08H27.63c-1.81 0-3.28-1.47-3.28-3.28V13.7c0-1.81 1.47-3.28 3.28-3.28h10.33c1.81 0 3.28 1.47 3.28 3.28v17.1c0 1.81-1.47 3.28-3.28 3.28ZM27.63 12.92c-.43 0-.78.35-.78.78v17.1c0 .43.35.78.78.78h10.33c.43 0 .78-.35.78-.78V13.7c0-.43-.35-.78-.78-.78H27.63ZM13.49 18.3H6.05c-1.54 0-2.79-1.25-2.79-2.79v-1.73c0-1.54 1.25-2.79 2.79-2.79h7.44c1.54 0 2.79 1.25 2.79 2.79v1.73c0 1.54-1.25 2.79-2.79 2.79Zm-7.44-4.81a.29.29 0 0 0-.29.29v1.73c0 .16.13.29.29.29h7.44c.16 0 .29-.13.29-.29v-1.73a.29.29 0 0 0-.29-.29H6.05ZM9.38 34.08H6.05c-1.54 0-2.79-1.25-2.79-2.79v-.52c0-1.54 1.25-2.79 2.79-2.79h3.33c1.54 0 2.79 1.25 2.79 2.79v.52c0 1.54-1.25 2.79-2.79 2.79Zm-3.33-3.61a.29.29 0 0 0-.29.29v.52c0 .16.13.29.29.29h3.33c.16 0 .29-.13.29-.29v-.52a.29.29 0 0 0-.29-.29H6.05ZM19.01 34.08h-3.33c-1.54 0-2.79-1.25-2.79-2.79v-.52c0-1.54 1.25-2.79 2.79-2.79h3.33c1.54 0 2.79 1.25 2.79 2.79v.52c0 1.54-1.25 2.79-2.79 2.79Zm-3.33-3.61a.29.29 0 0 0-.29.29v.52c0 .16.13.29.29.29h3.33c.16 0 .29-.13.29-.29v-.52a.29.29 0 0 0-.29-.29h-3.33ZM20.94 22.36H4.51a1.25 1.25 0 0 1 0-2.5h16.43a1.25 1.25 0 0 1 0 2.5ZM20.94 26.41H4.51a1.25 1.25 0 0 1 0-2.5h16.43a1.25 1.25 0 0 1 0 2.5Z" style="fill:#515962;stroke-width:0"/></svg>
     `,
     content: `<div class="container col-xxl-8 px-4 py-5">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
@@ -57,30 +39,6 @@ blockManager.add('bs-hero-left', {
                 <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
                 <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
             </div>
-        </div>
-    </div>
-</div>`
-});
-
-blockManager.add('bs-hero-shadow', {
-    category: 'Bootstrap - Heroes',
-    label: 'Hero - Shadows',
-    media: `
-    `,
-    content: `<div class="container my-5">
-    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-            <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Border hero with cropped image and shadows</h1>
-            <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world's
-                most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
-                extensive prebuilt components, and powerful JavaScript plugins.</p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Primary</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-            </div>
-        </div>
-        <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
-            <img class="rounded-lg-3" src="bootstrap-docs.png" alt="" width="720">
         </div>
     </div>
 </div>`
